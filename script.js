94a1fcd8166e7a7239df536f33fe94d17b7208f1
romulo <romulodmilani@gmail.com>
2023-02-20 12:03:38 -0300

    função que joga apenas uma rodada

--- script.js
+++ script.js
@@ -19,8 +19,33 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+//Função que joga apenas uma rodada do jogo
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == 'pedra' && computerSelection == 'pedra') {
+        return 'É um impate'
+    } else if (playerSelection == 'pedra' && computerSelection == 'papel') {
+        return 'Computador ganhou'
+    } else if (playerSelection == 'pedra' && computerSelection == 'tesoura') {
+        return 'Você ganhou!'
+    } else if (playerSelection == 'papel' && computerSelection == 'papel') {
+        return 'É um impate'
+    } else if (playerSelection == 'papel' && computerSelection == 'pedra') {
+        return 'Você ganhou!'
+    } else if (playerSelection == 'papel' && computerSelection == 'tesoura') {
+        return 'Computador ganhou'
+    } else if (playerSelection == 'tesoura' && computerSelection == 'tesoura') {
+        return 'É um empate'
+    } else if (playerSelection == 'tesoura' && computerSelection == 'pedra') {
+        return 'Computador ganhou'
+    } else if (playerSelection == 'tesoura' && computerSelection == 'papel') {
+        return 'Você ganhou!'
+    }
+}
+
+//Uma rodada do jogo
+const playerSelection = 'TesOura';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
